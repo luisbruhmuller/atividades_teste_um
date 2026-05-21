56725a2f833fcb6236b07210fa6090aac8dd32ce
luis <luis>
2026-05-21 12:06:17 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login com o banco</title>
+</head>
+
+<body>
+    <h2>
+        login com php
+    </h2>
+    <form action="" method="POST">
+        <label for="usuario">usuario</label>
+        <input type="text" name="usuario">
+        <br>
+        <label for="senha">senha</label>
+        <input type="password" name="senha">
+        <button type="submit">entrar</button>
+    </form>
+</body>
+
+</html>
